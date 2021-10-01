3617a09ea83ce6df2e9179fadd82064faf5e2691
geekedger <geekedger@gmail.com>
2021-10-01 18:29:44 +0300

    feat: resolve delete-digit task

--- src/st-delete-digit.js
+++ src/st-delete-digit.js
@@ -11,7 +11,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * For n = 152, the output should be 52
  *
  */
-export default function deleteDigit(/* n */) {
+export default function deleteDigit(n) {
   var s = String(n).split('').map(Number)
   return Math.max(...s.map((el,i)=>{var r = s.slice(); r.splice(i,1); return Number( r.join(''))}))
 }
